486be1951bbd879343cbd6b2a2af34fa23c63daa
cuba1526 <cuba1526@yahoo.com.tw>
2018-05-17 17:45:33 +0900

    V0.02_with_demo

---
+++ Assets/Scripts/ball_script.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ball_script : MonoBehaviour {
+
+public float speed_x;
+public float speed_y;
+
+public Text console;
+	// Use this for initialization
+	void Start () {
+		speed_x=Random.Range(0.1f,0.2f);
+		speed_y=Random.Range(0.1f,0.2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position += new Vector3 (speed_x, speed_y, 0f);
+	}
+	void OnTriggerEnter(Collider hit)
+	{
+		if(hit.tag=="Wall")
+		{
+			speed_y=-speed_y;
+		}
+		if(hit.tag=="Wall_left")
+		{
+			speed_x=0;
+			speed_y=0;
+			console.text="Right player win";
+		}
+		if(hit.tag=="Wall_right")
+		{
+			speed_x=0;
+			speed_y=0;
+			console.text="Left player win";
+		}
+		if(hit.tag=="Player")
+		{
+			speed_x=-speed_x;
+		}
+	}
+	
+}
---
+++ Assets/Scripts/ping_pong_player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ping_pong_player : MonoBehaviour {
+
+public GameObject player;
+public GameObject player_2;
+public GameObject ball;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.D) && player.transform.position.x <= -0.5) {
+			player.transform.position += new Vector3 (1.0f, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.A) && player.transform.position.x >= -9.8) {
+			player.transform.position -= new Vector3 (1.0f, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.W) && player.transform.position.y < 4) {
+			player.transform.position += new Vector3 (0, 1.0f, 0);
+		}
+		if (Input.GetKey (KeyCode.S) && player.transform.position.y > -4) {
+			player.transform.position -= new Vector3 (0, 1.0f, 0);
+		}
+		if (Input.GetKey (KeyCode.RightArrow) && player_2.transform.position.x <= 9.8) {
+			player_2.transform.position += new Vector3 (1.0f, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow) && player_2.transform.position.x >= 0.5) {
+			player_2.transform.position -= new Vector3 (1.0f, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.UpArrow) && player_2.transform.position.y < 4) {
+			player_2.transform.position += new Vector3 (0, 1.0f, 0);
+		}
+		if (Input.GetKey (KeyCode.DownArrow) && player_2.transform.position.y > -4) {
+			player_2.transform.position -= new Vector3 (0, 1.0f, 0);
+		}
+		if(Input.GetKey(KeyCode.R))
+		{
+			restart();
+		}
+	}
+
+	void restart()
+	{
+		ball.transform.position=new Vector3(0,0,0);
+		ball.GetComponent<ball_script>().speed_x=Random.Range(0.1f,0.2f);
+		ball.GetComponent<ball_script>().speed_y=Random.Range(0.1f,0.2f);
+		ball.GetComponent<ball_script>().console.text="";
+	}
+}
